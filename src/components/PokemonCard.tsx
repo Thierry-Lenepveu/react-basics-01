3bb1a5303f027230baf1e265926db9762b55fe8f
Thierry-Lenepveu <thierry.lenepveu.33@gmail.com>
2024-10-22 13:55:58 +0200

    add condition in react function.

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,7 +1,26 @@
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
 function PokemonCard() {
+    let pokemon = pokemonList[1];
+    if (pokemon.imgSrc) {
+        return <figure>
+            <figcaption>{pokemon.name}</figcaption>
+            <img src={pokemon.imgSrc} alt={pokemon.name}/>
+        </figure>;
+    }
+
     return <figure>
-        <figcaption>Bulbasaur</figcaption>
-        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur"/>
+        <figcaption>{pokemon.name}</figcaption>
+        <p>???</p>
     </figure>;
 }
 
