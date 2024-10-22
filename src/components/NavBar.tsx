e966d5e07d1fdf78d2bceb211c03c36285970b53
Thierry-Lenepveu <thierry.lenepveu.33@gmail.com>
2024-10-22 17:18:44 +0200

    Minor corrections.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -14,10 +14,12 @@ function NavBar({pokemonIndex, setPokemonIndex, pokemonList}: NavBarProps) {
         setPokemonIndex(pokemonIndex - 1)
       };
 
-    return <>
-    <button type="button" onClick={handleClickPrev} disabled={pokemonIndex <= 0}>Précédent</button>
-    <button type="button" onClick={handleClickNext} disabled={pokemonIndex >= pokemonList.length - 1}>Suivant</button>
-    </>;
+    return(
+    <>
+      <button type="button" onClick={handleClickPrev} disabled={pokemonIndex <= 0}>Précédent</button>
+      <button type="button" onClick={handleClickNext} disabled={pokemonIndex >= pokemonList.length - 1}>Suivant</button>
+    </>
+    );
 }
 
 export default NavBar;
