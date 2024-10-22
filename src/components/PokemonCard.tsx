4aa0c2213823499e3ebd99952065eb2b835b7832
Thierry-Lenepveu <thierry.lenepveu.33@gmail.com>
2024-10-22 14:56:58 +0200

    used prop pokemon in PokemonCard.

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,16 +1,6 @@
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
-function PokemonCard() {
-    let pokemon = pokemonList[1];
+function PokemonCard({ pokemon }) {
+
     if (pokemon.imgSrc) {
         return <figure>
             <figcaption>{pokemon.name}</figcaption>
