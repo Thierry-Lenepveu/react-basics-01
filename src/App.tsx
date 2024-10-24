691b33ff767826fb58a8900cc1044b1514e3ad84
Thierry-Lenepveu <thierry.lenepveu.33@gmail.com>
2024-10-24 15:30:43 +0200

    Added other pokemons.

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,30 @@ import './App.css'
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "pikachu",
-  },
-  {
-    name: "mew",
-  },
-];
-
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
 function App() {
   const [pokemonIndex, setIndex] = useState(0);
 
